9c9a1e90d73b6c1082038c0b2eb6f808e56ebdaf
George Roros <groros2@icloud.com>
2018-07-11 14:14:47 -0400

    Worked on forms

--- Forms/Hero/main.js
+++ Forms/Hero/main.js
@@ -1,6 +1,29 @@
 const form = document.forms['hero'];
 form.addEventListener('submit', makeHero, false);
 
+const label = form.querySelector('label');
+const error = document.createElement('div');
+error.classList.add('error');
+error.textContent = '! Your name is not allowed to start with X.';
+label.append(error);
+
+function validateInline() {
+  const heroName = this.value.toUpperCase();
+  if(heroName.startsWith('X')){
+    error.style.display = 'block';
+  } else {
+    error.style.display = 'none';
+  }
+}
+
+function validate(event){
+  const firstLetter = form.heroName.value[0];
+  if(firstLetter.toUpperCase() === 'X'){
+    event.preventDefault();
+    alert('Your name is not allowed to start with X!');
+  }
+}
+
 function makeHero(event) {
   event.preventDefault(); // prevent the form from being submitted
 
@@ -18,6 +41,14 @@ function makeHero(event) {
 
   //Refactor Above For Loop Usin Array Iterators
   hero.powers = [...form.powers].filter(box => box.checked).map(box => box.value);
+
+  hero.category = form.category.value;
+
+  hero.age = form.age.value;
+
+  hero.city = form.city.value;
+
+  hero.origin = form.origin.value;
 
   alert(JSON.stringify(hero));
 
--- quiz/main.js
+++ quiz/main.js
@@ -11,6 +11,7 @@ const view = {
   result: document.getElementById('result'),
   info: document.getElementById('info'),
   start: document.getElementById('start'),
+  response: document.querySelector('#response'),
   render(target, content, attributes){
     for(const key in attributes){
       target.setAttribute(key, attributes[key]);
@@ -22,50 +23,68 @@ const view = {
   },
   hide(element) {
     element.style.display = 'none';
+  },
+  setup(){
+    this.show(this.question);
+    this.show(this.response);
+    this.show(this.result);
+    this.hide(this.start);
+    this.render(this.score,game.score);
+    this.render(this.result,'');
+    this.render(this.info,'');
+    this.resetForm();
+  },
+  resetForm() {
+    this.response.answer.value = '';
+    this.response.answer.focus();
+  },
+  teardown() {
+    this.hide(this.question);
+    this.hide(this.response);
+    this.show(this.start);
   }
 };
 
 const game = {
   start(quiz){
-    this.questions = [...quiz]
     this.score = 0; // initialize score
-    view.hide(view.start); // hide start game button
-
-    // main game loop
-    for (const question of this.questions){
-      this.question = question
-      this.ask();
-    }
-    // end of main game loop
-
-    this.gameOver();
+    this.questions = [...quiz];
+    view.setup();
+    this.ask();
   },
 
   // function declarations
-  ask(){
-    const question = `What is ${this.question.name}'s real name?`;
-    view.render(view.question, question);
-    const response = prompt(question);
-    this.check(response);
+  ask(name){
+    if(this.questions.length > 0){
+      this.question = this.questions.pop();
+      const question = `What is ${this.question.name}'s real name?`;
+      view.render(view.question, question);
+    } else {
+      this.gameOver();
+    }
   },
 
-  check(response){
+  check(event){
+    event.preventDefault();
+    const response = view.response.answer.value;
     const answer = this.question.realName;
     if (response.toLowerCase() === answer.toLowerCase()){
       view.render(view.result, 'Correct!',{'class':'correct'});
-      alert('Correct!');
       this.score++;
       view.render(view.score, this.score);
     }else {
       view.render(view.result, `Wrong! The correct answer was ${answer}`, {'class': 'wrong'});
-      alert(`Wrong! The Correct answer was ${answer}`);
     }
+    view.resetForm();
+    this.ask();
   },
 
   gameOver(){
-    view.show(view.start); // show start game button
     view.render(view.info, `Game Over, you scored ${this.score} point${this.score !==1 ? 's' : ''}`);
+    view.teardown();
   },
 }
 
 view.start.addEventListener('click', () => game.start(quiz), false);
+view.response.addEventListener('submit', (event) => game.check(event), false);
+view.hide(view.response);
